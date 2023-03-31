af5c68f12ddf801c4ce9c07977255ec042dd255d
ap_boot <sahapri@gmail.com>
2023-03-31 16:05:18 -0400

    Fixed loop iterations in javascript file

--- script.js
+++ script.js
@@ -3,57 +3,61 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword(){
-  var lengthofpwd = prompt("how many characters would you like password to contain?");
+  var lengthofpwd = prompt("How many characters would you like the password to contain?");
 
   if (lengthofpwd <8 && lengthofpwd!== null) {
-     alert("password Length should atleast be 8 characters");
+     alert("Password length should atleast be 8 characters");
      writePassword();
   }
   else if (lengthofpwd >128){
-      alert("password length sould be between 8 -128 characters");
+      alert("Password length sould be between 8 - 128 characters");
       writePassword();
   }
   else if (lengthofpwd== null){
-      alert("Thank you for Tyring")
+      alert("Thank you for Trying!!")
   }
   else if(lengthofpwd>=8 || lengthofpwd <=128){
     var confirmspecialcharacters = confirm(" Click ok to confirm including Special Characters");
-    var confirmnumbers = confirm(" Click ok to confirm including numbers");
+    var confirmnumbers = confirm(" Click ok to confirm to include numbers");
     var confirmuppercaseletters = confirm(" Click ok to confirm including UPPERcase letters");
     var confirmlowercaseletters = confirm(" Click ok to confirm including lowercase letters");
-  }
 
-  var randompassword = [];
-  var splcharacters = ["!","@","#","$","%","&","*"];
-  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-  var upcaselet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var lowcaselett = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    if ((!confirmspecialcharacters)&&(!confirmnumbers)&&(!confirmuppercaseletters)&&(!confirmlowercaseletters)){
+      alert("Please select atleast one character type");
+      writePassword();
+    }
+  
+    var randompassword = [];
+    var splcharacters = ["!","@","#","$","%","&","*"];
+    var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+    var upcaselet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var lowcaselett = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-  for (var i=0;i< lengthofpwd; i){
-    console.log("<<index>>>",i)
+    for (var i=0;i< lengthofpwd; i){
       
-    if ((confirmspecialcharacters) && (i<lengthofpwd)){
+      if ((confirmspecialcharacters) && (i<lengthofpwd)){
         var index1= Math.floor(Math.random() * splcharacters.length);
         randompassword.push(splcharacters[index1]);
         i++;
-    }
-    if ((confirmnumbers) && (i<lengthofpwd)){
+      }
+      if ((confirmnumbers) && (i<lengthofpwd)){
          var index2= Math.floor(Math.random() * numbers.length);
         randompassword.push(numbers[index2]);
         i++;
-    }
-    if ((confirmuppercaseletters) && (i<lengthofpwd)){
+      }
+      if ((confirmuppercaseletters) && (i<lengthofpwd)){
         var index3= Math.floor(Math.random() * upcaselet.length);
         randompassword.push(upcaselet[index3]);
         i++;
-    }
-    if ((confirmlowercaseletters) && (i<lengthofpwd)){
+      }
+      if ((confirmlowercaseletters) && (i<lengthofpwd)){
         var index4= Math.floor(Math.random() * lowcaselett.length);
         randompassword.push(lowcaselett[index4]);
         i++;
-    }
-  }  
-  return (randompassword.join(""));
+      }
+   }  
+  }
+return (randompassword.join(""));
 }
 
 
